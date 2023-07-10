9d34d3f3473cb63809cb046a76985933a0c3482f
Sammy Weller <sammymermell@gmail.com>
2023-07-10 14:59:10 -0400

    updated passport.js with then/catch syntax

--- passport.js
+++ passport.js
@@ -14,27 +14,26 @@ let Users = Models.User,
     passwordField: 'password'
   }, (username, password, callback) => {
     console.log(username + '  ' + password);
-    Users.findOne({ username: username }, (error, user) => {
-      if (error) {
+    Users.findOne({ username: username })
+      .then((user) => {
+        if (!user) {
+          console.log('incorrect username');
+          return callback(null, false, { message: 'Incorrect username.' });
+        }
+  
+        if (!user.validatePassword(password)) {
+          console.log('incorrect password');
+          return callback(null, false, { message: 'Incorrect password.' });
+        }
+  
+        console.log('finished');
+        return callback(null, user);
+      })
+      .catch((error) => {
         console.log(error);
         return callback(error);
-      }
-  
-      if (!user) {
-        console.log('incorrect username');
-        return callback(null, false, {message: 'Incorrect username.'});
-      }
-  
-      if (!user.validatePassword(password)) {
-        console.log('incorrect password');
-        return callback(null, false, {message: 'Incorrect password.'});
-      }
-  
-      console.log('finished');
-      return callback(null, user);
-    });
+      });
   }));
-
 
 
 
