46b9bf4889f4277733f9307647585ce0ed9b931f
Sammy Weller <sammymermell@gmail.com>
2023-07-10 14:00:06 -0400

    edited cors

--- index.js
+++ index.js
@@ -23,7 +23,17 @@ const app = express(); //declares a variable that encapsulates Express’s funct
 
 //Allow requests from all domans:
 const cors = require('cors');
-app.use(cors());
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'https://sw-myflix-app-baa5e3f40824.herokuapp.com/'];
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 
 
